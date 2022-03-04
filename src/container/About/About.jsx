6343100042eae7c82a42792b0b417e54a7c8cd1d
Bahaa <bahaayoussof@gmail.com>
2022-03-04 15:35:48 +0200

    fetch about content from sanity

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,23 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 import { images } from "../../constants";
 import "./About.scss";
-
-const abouts = [
-	{
-		title: "Web Development",
-		description: "I have a solid foundation in web development.",
-		imgUrl: images.about01,
-	},
-	{
-		title: "Frontend Development",
-		description: "I am a good web developer using JS technologies.",
-		imgUrl: images.about04,
-	},
-];
+import { client, urlFor } from "../../client";
 
 const About = () => {
+	const [abouts, setAbouts] = useState([]);
+
+	useEffect(() => {
+		const query = '*[_type == "abouts"]';
+
+		client.fetch(query).then(data => setAbouts(data));
+	}, []);
+
 	return (
 		<>
 			<h2 className="head-text">
@@ -33,7 +29,7 @@ const About = () => {
 						className="app__profile-item"
 						key={about.title + index}
 					>
-						<img src={about.imgUrl} alt={about.title} />
+						<img src={urlFor(about.imgUrl)} alt={about.title} />
 						<h2 className="bold-text" style={{ marginTop: 20 }}>
 							{about.title}
 						</h2>
