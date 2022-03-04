574e414026277515f52854faaf13930f615845c8
Bahaa <bahaayoussof@gmail.com>
2022-03-04 14:38:29 +0200

    add About

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
-import './About.scss'
+import { images } from "../../constants";
+import "./About.scss";
+
+const abouts = [
+	{
+		title: "Web Development",
+		description: "I have a solid foundation in web development.",
+		imgUrl: images.about01,
+	},
+	{
+		title: "Frontend Development",
+		description: "I am a good web developer using JS technologies.",
+		imgUrl: images.about04,
+	},
+];
+
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+	return (
+		<>
+			<h2 className="head-text">
+				I Know that <span>Good Development</span> <br /> means <span>Good Business</span>
+			</h2>
 
-export default About
+			<div className="app__profiles">
+				{abouts.map((about, index) => (
+					<motion.div
+						whileInView={{ opacity: 1 }}
+						whileHover={{ scale: 1.1 }}
+						transition={{ duration: 0.5, type: "tween" }}
+						className="app__profile-item"
+						key={about.title + index}
+					>
+						<img src={about.imgUrl} alt={about.title} />
+						<h2 className="bold-text" style={{ marginTop: 20 }}>
+							{about.title}
+						</h2>
+						<p className="p-text" style={{ marginTop: 10 }}>
+							{about.description}
+						</p>
+					</motion.div>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default About;
