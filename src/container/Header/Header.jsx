0e01de7253ba994b113c48181c179bec6f8266db
Bahaa Youssof <bahaayoussof@gmail.com>
2022-03-13 14:40:37 +0200

    add Footer

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -60,7 +60,7 @@ const Header = () => {
 				whileInView={scaleVariants.whileInView}
 				className="app__header-circles"
 			>
-				{[images.redux, images.react_circle, images.sass].map((circle, index) => (
+				{[images.redux, images.react, images.sass].map((circle, index) => (
 					<div className="circle-cmp app__flex" key={`circle-${index}`}>
 						<img src={circle} alt="circle" />
 					</div>
