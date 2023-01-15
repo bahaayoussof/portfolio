af807666d855632ba8e48d0997a2dc5bf0e6376e
Bahaa Youssof <bahaayoussof@gmail.com>
2023-01-15 13:26:01 +0200

    fix navbar in mobile screen and add 2023 work experience

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -30,28 +30,30 @@ const Skills = () => {
 
 			<div className="app__skills-container">
 				<motion.div className="app__skills-list">
-					{skills.map(skill => (
-						<motion.div
-							whileInView={{ opacity: [0, 1] }}
-							transition={{ duration: 0.5 }}
-							className="app__skills-item app__flex"
-							key={skill.name}
-						>
-							<div className="app__flex" style={{ backgroundColor: skill.bgColor }}>
-								<img src={urlFor(skill.icon)} alt={skill.name} />
-							</div>
-							<p className="p-text">{skill.name}</p>
-						</motion.div>
-					))}
+					{skills.map(skill => {
+						return (
+							<motion.div
+								whileInView={{ opacity: [0, 1] }}
+								transition={{ duration: 0.5 }}
+								className="app__skills-item app__flex"
+								key={skill._id}
+							>
+								<div className="app__flex" style={{ backgroundColor: skill.bgColor }}>
+									<img src={urlFor(skill.icon)} alt={skill.name} />
+								</div>
+								<p className="p-text">{skill.name}</p>
+							</motion.div>
+						);
+					})}
 				</motion.div>
 				<div className="app__skills-exp">
 					{experiences.map(experience => (
-						<motion.div className="app__skills-exp-item" key={experience.year}>
+						<motion.div className="app__skills-exp-item" key={experience._id}>
 							<div className="app__skills-exp-year">
 								<p className="bold-text">{experience.year}</p>
 							</div>
 							<motion.div className="app__skills-exp-works">
-								{experience.works.map((work) => (
+								{experience.works.map(work => (
 									<>
 										<motion.div
 											whileInView={{ opacity: [0, 1] }}
@@ -59,7 +61,7 @@ const Skills = () => {
 											className="app__skills-exp-work"
 											data-tip
 											data-for={work.name}
-											key={work.name}
+											key={work._key}
 										>
 											<h4 className="bold-text">{work.name}</h4>
 											<p className="p-text">{work.company}</p>
