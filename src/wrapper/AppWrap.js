a930cbed756b97b94fb4efb8a1311cae07318942
bahaayoussof <bahaayoussof@gmail.com>
2025-08-22 21:48:48 +0300

    remove social links and navigation dots

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { NavigationDots, SocialMedia } from "../components";
 
 const AppWrap = (Component, idName, classNames) =>
-	function HOC() {
-		const date = new Date();
-		return (
-			<div id={idName} className={`app__container ${classNames}`}>
-				<SocialMedia />
+  function HOC() {
+    const date = new Date();
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
+        <div className="app__wrapper app__flex">
+          <Component />
 
-				<div className="app__wrapper app__flex">
-					<Component />
-
-					<div className="copyright">
-						<p className="p-text">Copyright &copy; {date.getFullYear()} Bahaa</p>
-						<p className="p-text">All rights reserved</p>
-					</div>
-				</div>
-				<NavigationDots active={idName} />
-			</div>
-		);
-	};
+          <div className="copyright">
+            <p className="p-text">
+              Copyright &copy; {date.getFullYear()} Bahaa
+            </p>
+            <p className="p-text">All rights reserved</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
 export default AppWrap;
